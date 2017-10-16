9f09fb1bb05356ceb5be58ce459432c8cf6d1ee8
Akeam williams <akeam.williams@yahoo.com>
2017-10-16 15:45:41 -0400

    add code to reset the maze

--- maze.js
+++ maze.js
@@ -10,19 +10,30 @@ let status=0;
 //make all boundaries glow red on hover
 let areas=document.querySelectorAll(".boundary");
      areas[0].addEventListener("mouseover",function () {
-       for(let i=1; i<6;i++)
+       for(let i=0; i<6;i++)
        {
          areas[i].classList.add("youlose");
        }
        status=1;
      });
-     
+
 //congratulate the user when them reach the end
 let end = document.getElementById("end");
     end.addEventListener("mouseover",function (){
     if(status===0)
     alert(" You win!");
     });
+
+// reset the game 
+let resetMaze = document.getElementById("start");
+    resetMaze.addEventListener("click",function () {
+    for(let i=0; i<6;i++)
+         {
+           areas[i].classList.remove("youlose");
+         }
+         status=0;
+       });
+
 
 }
 //load on the html file to full load before run
