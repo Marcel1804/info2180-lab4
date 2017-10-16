b05d287cf8521ade2a05cd72213be200d70ea5ca
Akeam williams <akeam.williams@yahoo.com>
2017-10-16 14:22:18 -0400

    add code to change colour of one section of the maze

---
+++ maze.js
@@ -0,0 +1,11 @@
+//the events handler for the maze
+window.onload = function(){
+
+//change the colour of one section(top-left) of the maze
+let colourChange = document.getElementById("boundary1");
+    colourChange.addEventListener("mouseover",function (){
+    colourChange.classList.add("youlose");});
+
+
+}
+//load on the html file to full load before run
