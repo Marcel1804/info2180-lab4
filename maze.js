313b657ec8e66da690d147b51c8acda03628b4e5
Akeam williams <akeam.williams@yahoo.com>
2017-10-16 14:50:29 -0400

    add code for all boundaries to glow red on hover

--- maze.js
+++ maze.js
@@ -6,6 +6,14 @@ let colourChange = document.getElementById("boundary1");
     colourChange.addEventListener("mouseover",function (){
     colourChange.classList.add("youlose");});
 
+//make all boundaries glow red on hover
+let areas=document.querySelectorAll(".boundary");
+     areas[0].addEventListener("mouseover",function () {
+       for(let i=1; i<6;i++)
+       {
+         areas[i].classList.add("youlose");
+       }
+     });
 
 }
 //load on the html file to full load before run
